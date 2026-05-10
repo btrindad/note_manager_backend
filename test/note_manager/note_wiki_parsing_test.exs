5e1fdeac11407a2b32f03e730a4fe8aadc4b16d9
Bernardo <fudog931@gmail.com>
2026-05-10 13:48:23 -0400

    moved test to focus on parsing content first

--- test/note_manager/note_wiki_parsing_test.exs
+++ test/note_manager/note_wiki_parsing_test.exs
@@ -1,6 +1,7 @@
 defmodule NoteManager.NoteWikiParsingTest do
   use NoteManager.DataCase, async: true
 
+  alias NoteManager.KnowledgeBase.Note.ContentParser
   alias NoteManager.KnowledgeBase, as: KG
 
   setup do
@@ -11,17 +12,33 @@ defmodule NoteManager.NoteWikiParsingTest do
 
       This is an example note that exercises some critical features
       like links to [real source](https://google.com) and links to
-      other [[notes|#{existing_note.id}]]. Non existant uuids are
+      other [[#{existing_note.id}|note]]. Non existant uuids are
       ignored [[#{Ecto.UUID.generate()}]].
+
+      UUIDs not in a wiki link are treated as content #{Ecto.UUID.generate()}
+      and Wiki Links that do not contain UUIDs are also ignored like
+      [[some other content 123|this]].
     """
 
     [note_content: note_with_links, target_note: existing_note]
   end
 
-  test "saves outgoing links to other notes", %{note_content: content, target_note: target} do
-    assert {:ok, %KG.Note{outgoing_neighbors: neigh}} = KG.new_note(%{content: content})
+  describe "parsing content" do
+    test "extracts all valid UUIDs in Wiki Links", %{
+      note_content: content,
+      target_note: %KG.Note{id: target_id}
+    } do
+      assert {:ok, links} = ContentParser.extract_links(content)
 
-    assert length(neigh) == 1
-    assert neigh == [target.id]
+      assert length(links) == 2
+      assert Enum.member?(links, target_id)
+    end
   end
+
+  # test "saves outgoing links to other notes", %{note_content: content, target_note: target} do
+  #   assert {:ok, %KG.Note{outgoing_neighbors: neigh}} = KG.new_note(%{content: content})
+
+  #   assert length(neigh) == 1
+  #   assert neigh == [target.id]
+  # end
 end
