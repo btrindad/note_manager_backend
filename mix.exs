79296406eaeaee49581720680f037a6e6af3b981
Bernardo <fudog931@gmail.com>
2026-05-10 22:55:29 -0400

    added a seed file for local demoing

--- mix.exs
+++ mix.exs
@@ -78,7 +78,8 @@ defmodule NoteManager.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ash.setup", "run priv/repo/seeds.exs"],
+      setup: ["deps.get", "ash.setup"],
+      seed: ["run priv/repo/seeds.exs"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ash.setup --quiet", "test"],
